9c662d8918a4fd931400013d90953cc623df2fd0
Rayane Ventura <rayyventura@gmail.com>
2022-03-22 07:47:58 +0100

    feat:signin and signup

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../contexts/AuthContext";
+import AuthContext from "../context/authContext";
 
 export default function useAuth() {
   return useContext(AuthContext);
