6e00358acf47222d95827b8f0b3169b7b1a4324b
blarth <jminocente@gmail.com>
2022-03-23 02:03:00 -0300

    fix: heroku baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://linktr-api.herokuapp.com/";
+const BASE_URL = "https://linktr-api.herokuapp.com";
 
 function createConfig(token) {
   return {
