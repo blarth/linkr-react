9b342d09fe49b3ae6dcce730812d0ef01f0474de
blarth <jminocente@gmail.com>
2022-04-01 16:51:40 -0300

    fix: rendering hashatag page

--- src/components/PostComponent/Shares/index.js
+++ src/components/PostComponent/Shares/index.js
@@ -14,7 +14,7 @@ import { useEffect, useState } from "react";
 import VectorRepost from "../../../assets/repost.png";
 import VectorReposted from "../../../assets/reposted.png";
 
-export default function Repost({ id, loadPost, loadHashTag, numberReposts }) {
+export default function   Repost({ id, loadPost, loadHashTag, numberReposts }) {
   const [alreadyReposted, setAlreadyReposted] = useState(false);
   const [modalIsOpen, setIsOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
